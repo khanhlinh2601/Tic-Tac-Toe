c913025c4e638eb47b84e7800ebf2473d76321b9
Khanh Linh Trinh <tklinh2601@gmail.com>
2022-02-05 09:07:06 +0700

    update history

--- index.js
+++ index.js
@@ -16,16 +16,13 @@ const WIN_CASES = [
   [2, 4, 6],
 ];
 let circleTurn;
-const state = ()=>{
-  return Array(9).fill(null)
-}
+const state = ()=>Array(9).fill(null)
 let historyState = [];
 historyState.push(state())
 let currentState
-let currentDataId=1;
-let currentStep=0;
+let currentDataId;
+let currentStep;
 
-//start game
 const startGame = () => {
   currentStep=0;
   currentDataId=1;
@@ -34,8 +31,6 @@ const startGame = () => {
     cell.classList.remove(O_CLASS);
     cell.addEventListener("click", handleTurn, { once: true });
   });
-
- 
   winMessage.classList.remove("show");
 };
 
@@ -45,23 +40,18 @@ const handleTurn = (e) => {
   const currentClass = circleTurn ? O_CLASS : X_CLASS;
   placeMark(cell, currentClass);
   if (checkWinner(currentClass)) {
-    endGame(false);
-    //false trả về O or X win
+    endGame(false); //x o win
   } else if (isDraw()) {
-    endGame(true);
-    //true trả về tie
+    endGame(true); //tie
   } else {
     changeTurn();
   }
   
-  //history
-  console.log(currentDataId)
   currentState = historyState[currentDataId-1].slice()
   currentState[cell.getAttribute("data-id")] = currentClass == "x-turn" ? "x" : "o"
   historyState.push(currentState)
-  currentDataId=historyState.length; //1 ham check current step
+  currentDataId=historyState.length; 
   currentStep+=1;
-  //div step ui
   const div = document.createElement("div")
   div.className = "step"
   div.setAttribute("data-id", historyState.length - 1)
@@ -87,17 +77,14 @@ const endGame = (draw) => {
   winMessage.classList.add("show");
 };
 
-//handle tie, full cell->tie->true
-const isDraw = () => {
+const isDraw = () => { //handle tie, full cell->tie->true
   return boardElements.every((cell) => {
     return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
   });
 };
 
-//change x and o
-const changeTurn = () => {
+const changeTurn = () => { //change x and o
   circleTurn = !circleTurn;
-
   if (!circleTurn) {
     currentPlayer.textContent = "X";
     currentPlayer.classList.remove(O_CLASS);
@@ -108,18 +95,15 @@ const changeTurn = () => {
   }
 };
 
-const checkWinner = (currentClass) => {
+const checkWinner = (currentClass) => { //kiểm tra từng trường hợp win -> từng case check từng ô xem có class ("x-turn" or "o-turn") đó ko
   return WIN_CASES.some((winCase) => {
     return winCase.every((index) => {
       return boardElements[index].classList.contains(currentClass);
     });
   });
 };
-//kiểm tra từng trường hợp win -> từng case check từng ô xem có class ("x-turn" or "o-turn") đó ko
+
 const render = (e)=>{
-  // console.log(historyState[e.target.getAttribute("data-id")])
-  // console.log(e.target.getAttribute("data-id"))
-  // console.log(e.target.getAttribute("step"))
   //remove về x o 
   boardElements.forEach((cell) => {
     cell.classList.remove(X_CLASS);
